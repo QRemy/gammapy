5d8b4a7b3aa4748c5ddeccd38b830209608ebcf7
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2024-06-04 11:09:14 +0200

    cleanup
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/estimators/map/core.py
+++ gammapy/estimators/map/core.py
@@ -75,16 +75,15 @@ VALID_QUANTITIES = [
     "npred",
     "npred_excess",
     "stat",
-    "stat_scan",
     "stat_null",
+    "stat_scan",
+    "stat_scan_local",
+    "norm_scan_values",
     "niter",
     "is_ul",
     "counts",
     "success",
     "n_dof",
-    "norm_scan_values",
-    "stat_scan_local",
-    "stat_scan",
 ]
 
 
@@ -95,14 +94,14 @@ OPTIONAL_QUANTITIES_COMMON = [
     "npred_excess",
     "stat",
     "stat_null",
+    "stat_scan",
+    "stat_scan_local",
+    "norm_scan_values",
     "niter",
     "is_ul",
     "counts",
     "success",
     "n_dof",
-    "norm_scan_values",
-    "stat_scan_local",
-    "stat_scan",
 ]
 
 
