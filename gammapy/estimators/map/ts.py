ba6e7aa5a39ff5644a3d4ef5cac4ea0b6bf399d8
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2024-07-03 10:11:12 +0200

    correct norm update
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/estimators/map/ts.py
+++ gammapy/estimators/map/ts.py
@@ -963,10 +963,6 @@ class BrentqFluxEstimator(Estimator):
         else:
             result = self.estimate_best_fit(dataset)
 
-        norm = result["norm"]
-        result["npred"] = dataset.npred(norm=norm).sum()
-        result["npred_excess"] = result["npred"] - dataset.npred(norm=0).sum()
-
         if "ul" in self.selection_optional:
             result.update(self.estimate_ul(dataset, result))
 
@@ -975,6 +971,11 @@ class BrentqFluxEstimator(Estimator):
 
         if "stat_scan" in self.selection_optional:
             result.update(self.estimate_scan(dataset, result))
+
+        norm = result["norm"]
+        result["npred"] = dataset.npred(norm=norm).sum()
+        result["npred_excess"] = result["npred"] - dataset.npred(norm=0).sum()
+        result["stat"] = dataset.stat_sum(norm=norm)
 
         return result
 
